cb6ce3e4bdf0afc146525788bf878b56a5c4d975
Frank <frank.fang@nokia.com>
2016-09-09 15:59:47 +0800

    Authentication step 1: add user model, login view and auth utility script

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ var config = {
   // mongodb config
   db: 'mongodb://127.0.0.1/apace_dev',
 
+  auth_cookie_name: '__apace_ap_tracker__',
 };
 
 if (process.env.NODE_ENV === 'test') {
---
+++ models/user.js
@@ -0,0 +1,42 @@
+var mongoose  = require('mongoose');
+var BaseModel = require("./base_model");
+var Schema    = mongoose.Schema;
+
+var UserSchema = new Schema({
+  uid: { type: Number },      // employeeNumber or uidNumber
+  name: { type: String},      // nsnPreferredName + sn
+  loginname: { type: String}, // nsnDomainId or uid
+  // pass: { type: String },
+  email: { type: String},     // mail
+  mobile: { type: String},    // mobile
+
+  cc: { type: String},        // nsnCostCentre
+  team: { type: String},      // nsnTeamName
+  biz_unit: { type: String }, // ou
+
+  // signature: { type: String },
+  // profile: { type: String },
+  
+  is_block: {type: Boolean, default: false },  
+  active: { type: Boolean, default: false },  // nsnStatus
+
+  receive_reply_mail: {type: Boolean, default: false },
+  receive_at_mail: { type: Boolean, default: false },
+  
+  create_at: { type: Date, default: Date.now },
+  update_at: { type: Date, default: Date.now },  
+});
+
+UserSchema.plugin(BaseModel);
+
+UserSchema.virtual('isTdd').get(function () {
+  // 积分高于 700 则认为是高级用户
+  return this.biz_unit === 'MBB TD LTE';
+});
+
+UserSchema.index({loginname: 1}, {unique: true});
+UserSchema.index({email: 1}, {unique: true});
+UserSchema.index({uid: 1});
+UserSchema.index({team: 1});
+
+mongoose.model('User', UserSchema);
